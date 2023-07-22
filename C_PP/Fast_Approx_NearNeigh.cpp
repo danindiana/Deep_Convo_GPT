d666981250c1289cea8464561e88bd665b330a58
danindiana <danindiana@users.noreply.github.com>
2023-07-22 06:41:54 -0500

    Create Fast_Approx_NearNeigh.cpp
    Please note that the optimizeEdge, calculateDistance, and checkMRNG functions still need to be implemented according to the desired behavior for your specific graph application. Replace the placeholder implementation in those functions with the appropriate code for your use case. The main function demonstrates an example usage of the combined algorithm.

---
+++ C_PP/Fast_Approx_NearNeigh.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <limits>
+
+// Function to calculate the distance between two vertices
+double calculateDistance(int v1, int v2) {
+    // Implementation of the distance calculation
+    // Replace this with the actual distance calculation method
+    // For example, you could use Euclidean distance or any other metric suitable for your graph.
+    return 0.0;
+}
+
+// Function to check if an edge between v1 and v2 is MRNG conform
+bool checkMRNG(const std::vector<std::vector<int>>& G, int v1, int v2) {
+    // Implementation of the checkMRNG algorithm
+    // Replace this with the actual checkMRNG implementation
+    // For example, you could check if the edge between v1 and v2 satisfies certain conditions.
+    return true;
+}
+
+// Function to optimize the edge (v1, v2) in the graph G
+void optimizeEdge(std::vector<std::vector<int>>& G, int v1, int v2, int iopt, size_t kopt, double eopt) {
+    // Implementation of the optimizeEdge algorithm
+    // Replace this with the actual optimizeEdge implementation
+    // For example, you could try swapping the edge (v1, v2) with the edge that improves the average neighbor distance.
+}
+
+// Function to perform dynamicEdgeOptimization
+void dynamicEdgeOptimization(std::vector<std::vector<int>>& G, int iopt, size_t kopt, double eopt) {
+    // Get a random vertex from G
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, G.size() - 1);
+    int v1 = dis(gen);
+
+    std::vector<int> N = G[v1]; // Neighbors of v1
+
+    for (int v2 : N) {
+        if (!checkMRNG(G, v1, v2)) {
+            optimizeEdge(G, v1, v2, iopt, kopt, eopt);
+        }
+    }
+
+    v1 = dis(gen);
+    N = G[v1]; // Neighbors of v1
+
+    int v2 = -1;
+    double maxEdgeDistance = -std::numeric_limits<double>::infinity();
+
+    for (int n : N) {
+        double edgeDistance = calculateDistance(v1, n);
+        if (edgeDistance > maxEdgeDistance) {
+            maxEdgeDistance = edgeDistance;
+            v2 = n;
+        }
+    }
+
+    if (v2 != -1) {
+        optimizeEdge(G, v1, v2, iopt, kopt, eopt);
+    }
+}
+
+int main() {
+    // Example usage
+    std::vector<std::vector<int>> G = {
+        {0, 1, 1, 0, 0},
+        {1, 0, 1, 1, 1},
+        {1, 1, 0, 1, 0},
+        {0, 1, 1, 0, 1},
+        {0, 1, 0, 1, 0}
+    };
+
+    int iopt = 5; // Max number of changes
+    size_t kopt = 3; // Number of search results
+    double eopt = 0.2; // Search range factor
+
+    dynamicEdgeOptimization(G, iopt, kopt, eopt);
+
+    // Output the optimized graph
+    std::cout << "Optimized Graph:" << std::endl;
+    for (const auto& row : G) {
+        for (const auto& val : row) {
+            std::cout << val << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
