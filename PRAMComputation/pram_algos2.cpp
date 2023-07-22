67a4575ffcb99a41375a515b2e245aa710c2192a
danindiana <danindiana@users.noreply.github.com>
2023-07-22 06:53:34 -0500

    Create pram_algos2.cpp
    Sure! Here's the cleaned copypasta and a C++ implementation:
    
    Copypasta:
    Algorithm 1 Returns the new value B' (x), if the label of node v is set to σ.
    procedure evaluate-state(v, σ, x)
    if anc-or-self(x, v) then ▷ v only influences x if x is an ancestor of v
    p ← At(s, first-child(v), last-child(v)) ▷ This is s, if v has no children
    q ← δ(p, σ)
    return Bt(q, v, x) ▷ If v = x this is q
    else
    return ρt(x)
    
    Algorithm 2 Returns the new value for At(p, x, y), for siblings x ⪯ y, if the label of node v is set to σ.
    procedure evaluate-sequence(v, σ, p, x, y)
    u ← parent(x) ▷ If x is a root then u is the super-root...
    if anc(u, v) AND anc-index(u, v) ≥ anc-index(u, x) AND anc-index(u, v) ≤ anc-index(u, y) then
    w ← child(u, anc-index(u, v))
    p1 ← At(p, x, left-sibling(w))
    p2 ← δA(p1, evaluate-state(v, σ, w))
    return At(p2, right-sibling(w), y)
    else
    return At(p, x, y) ▷ In this case the change at v is irrelevant
    
    Algorithm 3 Returns the new value for Bt(q, y, x), for an ancestor x of a node y if the label of node v is set to σ.
    procedure evaluate-path(v, σ, q, x, y)
    if anc(y, v) OR NOT anc(x, v) then
    return Bt(q, y, x)
    ▷ If v is a descendant of y or not a descendant of x, nothing changes
    u ← lca(v, y) ▷ u is on the path from y to x
    if u = v then
    z ← anc-child(u, y)
    p1 ← At(s, first-child(u), left-sibling(z))
    p2 ← δA(p1, Bt(q, y, z))
    q' ← δ(At(p2, right-sibling(z), last-child(u)), σ)
    else
    z ← anc-child(u, y)
    z' ← anc-child(u, v)
    if z ≺ z' then ▷ v is to the right of y in t
    p1 ← At(s, first-child(u), left-sibling(z))
    p2 ← δA(p1, Bt(q, y, z))
    p3 ← At(p2, right-sibling(z), left-sibling(z'))
    p4 ← δA(p3, evaluate-state(v, σ, z'))
    q' ← δ(At(p4, right-sibling(z'), last-child(u)), u))
    else ▷ If z' ≺ z, the algorithm proceeds analogously
    return Bt(q', u, x)
    
    C++ Implementation (sample structure):
    ```cpp
    #include <iostream>
    #include <vector>
    
    // Define your data structures and helper functions here (e.g., tree node, At function, etc.)
    
    // Algorithm 1: Returns the new value B'(x), if the label of node v is set to σ.
    // Procedure evaluate-state(v, σ, x)
    // Implementation of evaluate-state algorithm here...
    
    // Algorithm 2: Returns the new value for At(p, x, y), for siblings x ⪯ y, if the label of node v is set to σ.
    // Procedure evaluate-sequence(v, σ, p, x, y)
    // Implementation of evaluate-sequence algorithm here...
    
    // Algorithm 3: Returns the new value for Bt(q, y, x), for an ancestor x of a node y if the label of node v is set to σ.
    // Procedure evaluate-path(v, σ, q, x, y)
    // Implementation of evaluate-path algorithm here...
    
    int main() {
        // Sample usage of the PRAM algorithms
        // Define your data structures and tree here...
    
        // Call the PRAM algorithms with appropriate arguments to evaluate state, sequence, and path
    
        return 0;
    }
    ```
    
    File Name: pram_algorithms.cpp
    
    Please note that the actual implementation of `evaluate-state`, `evaluate-sequence`, and `evaluate-path` algorithms depends on your data structures and the specific logic you want to implement for your tree-based PRAM algorithms. Replace the comments inside the `main()` function with your actual tree data structure and call the corresponding PRAM algorithms as required.

---
+++ PRAMComputation/pram_algos2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+// Define your data structures and helper functions here (e.g., tree node, At function, etc.)
+
+// Algorithm 1: Returns the new value B'(x), if the label of node v is set to σ.
+// Procedure evaluate-state(v, σ, x)
+// Implementation of evaluate-state algorithm here...
+
+// Algorithm 2: Returns the new value for At(p, x, y), for siblings x ⪯ y, if the label of node v is set to σ.
+// Procedure evaluate-sequence(v, σ, p, x, y)
+// Implementation of evaluate-sequence algorithm here...
+
+// Algorithm 3: Returns the new value for Bt(q, y, x), for an ancestor x of a node y if the label of node v is set to σ.
+// Procedure evaluate-path(v, σ, q, x, y)
+// Implementation of evaluate-path algorithm here...
+
+int main() {
+    // Sample usage of the PRAM algorithms
+    // Define your data structures and tree here...
+
+    // Call the PRAM algorithms with appropriate arguments to evaluate state, sequence, and path
+
+    return 0;
+}
