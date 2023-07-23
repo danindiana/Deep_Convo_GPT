54cfff5414362f6b787137f5cad4b760e8271efe
danindiana <danindiana@users.noreply.github.com>
2023-07-23 18:19:31 -0500

    Create Four_Noble_Truths.cpp

---
+++ Buddhism/Four_Noble_Truths.cpp
@@ -0,0 +1,32 @@
+// First Noble Truth: Dukkha (Suffering)
+function firstNobleTruth() {
+    // Suffering exists in life.
+    cout << "First Noble Truth: Suffering exists in life." << endl;
+}
+
+// Second Noble Truth: Samudaya (Origin of Suffering)
+function secondNobleTruth() {
+    // The origin of suffering is attachment and craving.
+    cout << "Second Noble Truth: The origin of suffering is attachment and craving." << endl;
+}
+
+// Third Noble Truth: Nirodha (Cessation of Suffering)
+function thirdNobleTruth() {
+    // There is a way to end suffering by overcoming attachment and craving.
+    cout << "Third Noble Truth: There is a way to end suffering by overcoming attachment and craving." << endl;
+}
+
+// Fourth Noble Truth: Magga (Path to the Cessation of Suffering)
+function fourthNobleTruth() {
+    // The Noble Eightfold Path is the way to end suffering and attain liberation.
+    cout << "Fourth Noble Truth: The Noble Eightfold Path is the way to end suffering and attain liberation." << endl;
+}
+
+// Main program
+function main() {
+    // Present the Four Noble Truths step by step.
+    firstNobleTruth();
+    secondNobleTruth();
+    thirdNobleTruth();
+    fourthNobleTruth();
+}
